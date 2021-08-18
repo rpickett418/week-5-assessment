5832e899fbb0e441eee5c77a361d8cd52be453a1
rpickett418 <86981398+rpickett418@users.noreply.github.com>
2021-08-18 08:08:37 -0600

    update1

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+let color = "";
 const app = express();
 
 
@@ -36,19 +36,25 @@ app.get("/api/fortune", (req, res) => {
   res.status(200).send(randomFortunes);
 });
 
-app.get("/api/birthday/", (req,res) => {
+app.get("/api/birthday", (req,res) => {
   const birthdayFortunes = [
     "Your desires guide you towards the goals you should be pursuing!",
     "The energy that guides you will put you in touch with the motivating force that draws your individual expression.",
     "Staying active gives you a strong will to overcome any obstacle, including self-doubt!",
     "You realize how much negative emotions affect you, whether they are your own or that of others.",
-    "Purity of mind and body are important for maintaining internal clarity.",
+    "Purity of mind and body are important for maintaining internal clarity."
   ];
   let randomIndex = Math.floor(Math.random() * birthdayFortunes.length);
   let randomBirthdayFortunes = birthdayFortunes[randomIndex];
 
   res.status(200).send(randomBirthdayFortunes);
 });
+
+app.post("/api/moodcolor", (req, res) => {
+  // (req.body will always be body)
+  console.log(req.body)
+  color = req.body.color
+})
 
 
 
